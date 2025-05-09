b163f6e2da7ccd7f764b6b2eb19094c50c1e2117
strivermedia <strivermedia1@gmail.com>
2025-05-09 15:01:57 -0700

    green

--- packages/blog-starter-kit/themes/enterprise/tailwind.config.js
+++ packages/blog-starter-kit/themes/enterprise/tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
 				'accent-7': '#333',
 				success: '#0070f3',
 				cyan: '#79FFE1',
-				primary: colors.seafoam,
+				primary: colors.green,
 			},
 			typography: () => ({
 				DEFAULT: {
