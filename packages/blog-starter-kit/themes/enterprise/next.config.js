9d87fef2cc9bed33d59837d71f9dfdb3c54c4f88
strivermedia <strivermedia1@gmail.com>
2025-05-16 13:32:50 -0700

    blog 8

--- packages/blog-starter-kit/themes/enterprise/next.config.js
+++ packages/blog-starter-kit/themes/enterprise/next.config.js
@@ -74,22 +74,6 @@ const config = {
 		],
 	},
 	async rewrites() {
-		return [
-			{
-				source: '/ping/data-event',
-				destination: `${ANALYTICS_BASE_URL}/api/data-event`,
-			},
-			{
-				source: '/api/analytics',
-				destination: `${HASHNODE_ADVANCED_ANALYTICS_URL}/api/analytics`,
-			},
-		];
-	},
-	async redirects() {
-		return await getRedirectionRules();
-	},
-	};
-	async rewrites() {
     return [
       {
         source: "/blog",
